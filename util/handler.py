0ecbe542131e0b64ed8949a731b9c1e054d9cb84
Shu Guang Bo <shugb@cn.ibm.com>
2018-02-24 22:26:25 +0800

    added new files

---
+++ util/handler.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = ['"shuguangbo" <1262448230@qq.com>']
+
+import logging
+import traceback
+import pypinyin
+from pypinyin import pinyin, load_phrases_dict, lazy_pinyin, slug, TONE
+from poem.poem import sc
+import json
+import jieba
+
+from base.handler import BaseHandler
+
+class restGetPinyinHandler(BaseHandler):
+    def __init__(self, application, request, **kwargs):
+        super(restGetPinyinHandler, self).__init__(application, request, **kwargs)
+
+    def get(self, *args, **kwargs):
+        text = self.get_argument("text", "")
+        try :
+            pinyin = self.pinyin(text)
+        except Exception as e:
+            import traceback
+            stack = traceback.format_exc()
+            logging.error("get pinyin failed - error:%s cause:%s"
+                          % ( e, stack))
+            self.write_error(404, reason="Internal Error")
+        else:
+            self.write(pinyin)
+
+    def pinyin(self, text) :
+        """ 将一段文字转换为拼音，以JSON格式返回 
+            返回数组中的每一个元素为对应文字的拼音，多音字以数组列出所有拼音
+            中心 [['zhōng', 'zhòng'], ['xīn']]
+        """
+        text_length = len(text.strip());
+        if (text_length == 0) : return []
+#        print "input: %d %s" % (text_length, text)
+        ptext = {'duo_pinyin': pinyin(text, heteronym=True, errors=u'ignore'),
+               'dan_pinyin': lazy_pinyin(text, style=pypinyin.TONE, errors=u'ignore') }
+#        str = json.dumps(pinyin(text, heteronym=True), ensure_ascii=False, encoding='utf-8')
+        str = json.dumps(ptext, ensure_ascii=False, encoding='utf-8')
+#        print "output: %s" % str
+        return str
+
+class restGetCommentHandler(BaseHandler):
+    def __init__(self, application, request, **kwargs):
+        super(restGetCommentHandler, self).__init__(application, request, **kwargs)
+
+    def get(self, *args, **kwargs):
+        text = self.get_argument("text", "")
+        try :
+            comment = self.comment(text)
+        except Exception as e:
+            import traceback
+            stack = traceback.format_exc()
+            logging.error("get comment failed - error:%s cause:%s"
+                          % ( e, stack))
+            self.write_error(404, reason="Internal Error")
+        else:
+            self.write(comment)
+
+    def comment(self, text) :
+        """ 将一段文字分词，在注释库中找出对应注释，以JSON格式返回
+            返回数组中的每一个元素为对应文字的注释
+            [{关键字1：注释1},{关键字2：注释2},...]
+        """
+        text_length = len(text.strip());
+        print "input: %d %s" % (text_length, text)
+
+        clist = []; 
+
+        if (text_length > 0) : 
+            for key in jieba.cut_for_search(text) :
+#                print "key: %s" % key
+                if sc.comment.has_key(key) :
+#                    print "key: %s comment: %s" % (key, sc.comment[key])
+                    clist.append({key: sc.comment[key]})
+
+        str = json.dumps(clist, ensure_ascii=False, encoding='utf-8')
+        print "output: %s" % str
+        return str
